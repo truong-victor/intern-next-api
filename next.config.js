3d2f96fe68a56080fe8309bc29af55ab190077bd
victor <truongpham2412.nd@gmail.com>
2023-11-08 09:29:54 +0700

    update config

--- next.config.js
+++ next.config.js
@@ -1,24 +1,45 @@
 const nextConfig = {
-	publicRuntimeConfig: {
-		favicon: './public/favicon.svg'
-	},
-	serverRuntimeConfig: {
-		images: {
-			domains: ['ohaomxltnhpdriahjpvz.supabase.co'],
-			formats: ['image/webp']
-		}
-	},
-	reactStrictMode: false,
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'ohaomxltnhpdriahjpvz.supabase.co',
-				port: '',
-				pathname: '/storage/v1/object/public/itx_storage/**'
-			}
-		]
-	}
-}
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+  publicRuntimeConfig: {
+    favicon: "./public/favicon.svg",
+  },
+  serverRuntimeConfig: {
+    images: {
+      domains: ["ohaomxltnhpdriahjpvz.supabase.co"],
+      formats: ["image/webp"],
+    },
+  },
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ohaomxltnhpdriahjpvz.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/itx_storage/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
